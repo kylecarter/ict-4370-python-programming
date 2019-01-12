f61650f627406f457115f94a6802262e2cf79f5b
kylecarter <kyle.andrew.carter@gmail.com>
2019-01-11 18:42:48 -0600

    Adding initial files.

---
+++ one/carterk_assighment1.py
@@ -0,0 +1,38 @@
+'''
+/**
+ * Assignment 1 | ICT 4370 | Python Programming
+ *
+ * This represents the code work for addignment one. Requirements:
+ *
+ * Create a program that will ask the user for their name, a stock symbol for
+ * the stock they own (for example for Google that would be GOOGL), how many
+ * shares they own, what price they bought it at, and its current price.
+ * Calculate how much money they earned or lost. The output should look
+ * something like this:
+ *
+ * Stock ownership for Bob Smith
+ * ----------------------------------------------------
+ * GOOGL: 125 shares
+ * Purchase Price: 772.88
+ * Current Price per Share: 941.53
+ * Earnings/Loss to-date: $21081.25
+ *
+ * This assignment will provide you with the opportunity to break down a task
+ * with a specific business need into programmable tasks. While working on this
+ * assignment, you will have the opportunity to practice working with different
+ * types of variables, simple math operations and print statements.
+ *
+ * As a part of best programming practices, include code comments, at the
+ * minimum a header with your name, the date the program was written and a
+ * short description.
+ *
+ * @category   Assignment
+ * @package    AssignmentOne
+ * @author     Kyle A. Carter <kyle.carter@du.edu>
+ * @copyright  2019 Kyle A. Carter
+ * @license    https://opensource.org/licenses/GPL-3.0  GNU General Public License version 3
+ * @version    Git: 1.0.0
+ * @link       http://pear.php.net/package/PackageName
+ * 
+ */
+'''
