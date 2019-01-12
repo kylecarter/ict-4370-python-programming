f0443e67b0d8135a01b48473ede6409e566d2362
kylecarter <kyle.andrew.carter@gmail.com>
2019-01-11 20:00:10 -0600

    Adding updates to assignment 1.

--- one/carterk_assighment1.py
+++ one/carterk_assighment1.py
@@ -2,7 +2,8 @@
 /**
  * Assignment 1 | ICT 4370 | Python Programming
  *
- * This represents the code work for addignment one. Requirements:
+ * This represents the code work for addignment one. Note this project
+ * was written for Python 3 Requirements:
  *
  * Create a program that will ask the user for their name, a stock symbol for
  * the stock they own (for example for Google that would be GOOGL), how many
@@ -32,7 +33,79 @@
  * @copyright  2019 Kyle A. Carter
  * @license    https://opensource.org/licenses/GPL-3.0  GNU General Public License version 3
  * @version    Git: 1.0.0
- * @link       http://pear.php.net/package/PackageName
+ * @link       https://github.com/kylecarter/ict-4370-python-programming
  * 
  */
 '''
+
+import locale
+
+class StockInfo:
+
+    def __init__(self):
+        self.stock_name = ''
+        self.num_shares = float(0)
+        self.cost_per_share = float(0)
+        self.current_cost_per_share = float(0)
+        self.locale = locale.getdefaultlocale()
+
+    def get_data(self):
+        return {
+            'stock_name': input("Please enter a stock symbol you own: "),
+            'num_shares': input(
+                "How many shares of this stock do you own? "),
+            'cost_per_share': input(
+                "How much did you pay for each share? "),
+            'current_cost_per_share': input(
+                "How much do the shares cost now? ")
+        }
+
+    def valid(self, data):
+        valid = True
+        self.stock_name = data['stock_name']
+
+        try:
+            self.num_shares = float(data['num_shares'])
+        except:
+            valid = False
+        
+        try:
+            self.cost_per_share = self.convert_to_number(
+                data['cost_per_share'])
+        except:
+            valid = False
+
+        try:
+            self.current_cost_per_share = self.convert_to_number(
+                data['current_cost_per_share'])
+        except:
+            valid = False
+
+        return valid
+
+    def convert_to_number(self, currency):
+        locale.setlocale(locale.LC_ALL, self.locale[0] + '.' + self.locale[1])
+        conv = locale.localeconv()
+        return locale.atof(currency.strip(conv['currency_symbol']))
+
+    def get_stock_risk(self, purchase_price, current_price):
+        return self.get_local_currency(purchase_price - current_price)
+
+    def get_local_currency(self, currency):
+        return str(currency)
+
+    def show_data(self):
+        data = self.get_data()
+
+        if self.valid(data):
+            print(self.stock_name.upper() + ': ' + str(self.num_shares))
+            print('Purchase Price: ' + self.get_local_currency(self.cost_per_share))
+            print('Current Price per Share: ' +
+                  self.get_local_currency(self.current_cost_per_share))
+            print('Earnings/Loss to-date: ' +
+                self.get_stock_risk(self.cost_per_share, self.current_cost_per_share))
+        else:
+            print('There were issues with the data you entered. Please review your data entries.')
+
+assignment1 = StockInfo()
+assignment1.show_data()
